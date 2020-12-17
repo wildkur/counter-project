cd1e8684af9baad092c0a706b71b1655300c1451
Oscar Miras <oscar.miras@gmail.com>
2020-12-17 13:00:14 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-// Tu código aquí
+// Tu código aquí. Solución de Didac
+
+let increaseButton = document.querySelector('.nextBtn');
+let decreaseButton = document.querySelector('.prevBtn');
+
+let count = 0;
+let counter = document.querySelector('#counter');
+
+increaseButton.addEventListener('click', function() {
+    count++;
+    updateCounter(count);
+
+})
+
+decreaseButton.addEventListener('click', function() {
+    count--;
+    updateCounter(count);
+
+
+  
+})
+
+function updateCounter (count) {
+    counter.innerHTML = count;
+    
+    if(count < 0) {
+        counter.style.color = "red"
+    }
+    else if(count > 0) {
+        counter.style.color = "green"
+    }
+    else counter.style.color = "black"
+
+}
